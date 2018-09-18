c344ef1954ddd24a49c910e132f66235f2e238be
Aluno <you@example.com>
2018-09-18 12:07:12 -0300

    bota expandir feito

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,13 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 const botoes = document.querySelectorAll(".botao-expandir-retrair");
-debugger;
+
+botoes.forEach(function(botao){
+    botao.addEventListener("click",expandir);
+});
+
+function expandir(e){
+  let pai = e.currentTarget.parentNode;
+  // debugger;
+  pai.classList.toggle('expandido');
+}
