1e4fb98f89a84e9c30dc120587e76f3e9d1c6e60
Luiz Flávio <costadelima@gmail.com>
2018-09-24 14:48:26 -0300

    exercicio1 feito no exercicio2. Arquivos arrumados

--- exercicio1.js
+++ exercicio1.js
@@ -1,5 +1,6 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+/*
 const botoes = document.querySelectorAll(".botao-expandir-retrair");
 
 botoes.forEach(function(botao){
@@ -9,5 +10,41 @@ botoes.forEach(function(botao){
 function expandir(e){
   let pai = e.currentTarget.parentNode;
   // debugger;
+  pai.classList.toggle('expandido');
+}
+*/
+
+
+// botoes[0].addEventListener('click', myClick);
+//
+// function myClick(event){
+//   // console.log(event.target.className);
+//   alert('teste');
+// }
+
+// botoes[0].addEventListener('click', alert('teste'));
+////////////////////////////////////////////////////////////////////////////////
+//Exercício 1.1
+////////////////////////////////////////////////////////////////////////////////
+let botoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let x of botoes){
+  x.addEventListener('click', myClick);
+}
+
+function myClick(event){
+  alert('mensagem aqui');
+}
+////////////////////////////////////////////////////////////////////////////////
+//Exercício 1.2
+////////////////////////////////////////////////////////////////////////////////
+let botoesExpan = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let x of botoesExpan){
+  x.addEventListener('click', myClick);
+}
+
+function myClick(event){
+  let pai = event.currentTarget.parentNode;
   pai.classList.toggle('expandido');
 }
--- exercicio2.js
+++ exercicio2.js
@@ -1,52 +1,11 @@
-// Faça o exercício dos parágrafos aqui
+// Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
-/*
-const botoes = document.querySelectorAll(".botao-expandir-retrair");
 
-botoes.forEach(function(botao){
-    botao.addEventListener("click",expandir);
-});
-
-function expandir(e){
-  let pai = e.currentTarget.parentNode;
-  // debugger;
-  pai.classList.toggle('expandido');
-}
-*/
-
-
-// botoes[0].addEventListener('click', myClick);
-//
-// function myClick(event){
-//   // console.log(event.target.className);
-//   alert('teste');
-// }
-
-// botoes[0].addEventListener('click', alert('teste'));
-////////////////////////////////////////////////////////////////////////////////
-//Exercício 1.1
-////////////////////////////////////////////////////////////////////////////////
-let botoes = document.querySelectorAll(".botao-expandir-retrair");
-
-for (let x of botoes){
-  x.addEventListener('click', myClick);
-}
-
-function myClick(event){
-  alert('mensagem aqui');
-}
-////////////////////////////////////////////////////////////////////////////////
-//Exercício 1.2
-////////////////////////////////////////////////////////////////////////////////
-let botoesExpan = document.querySelectorAll(".botao-expandir-retrair");
-
-for (let x of botoesExpan){
-  x.addEventListener('click', myClick);
-}
-
-function myClick(event){
-  let pai = event.currentTarget.parentNode;
-  pai.classList.toggle('expandido');
-}
-
-
+const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
+  todasAsImagens = [
+    'philae-parts.jpg',
+    'philae-rosetta.jpg',
+    'philae-separation.jpg',
+    'philae-67-picture.jpg',
+    'philae-collecting.jpg'
+  ];
